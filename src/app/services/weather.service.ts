074335bf3e9985f66fcf413394923c14859f1adb
matthewlukyanov19 <brucedog9@gmail.com>
2024-07-27 14:04:11 +0100

    Created weather.service typescript to fetch weather data using an API from site 'https://home.openweathermap.org/api_keys', also updated home.page.ts to use WeatherService

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  private apiKey = '31a5be9ce67db08625ef86f8931da337'; 
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+    return this.http.get<any>(url);
+  }
+}
