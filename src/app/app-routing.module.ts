80797de6bde6ecee9d010f9346330bb61ed9c923
matthewlukyanov19 <brucedog9@gmail.com>
2024-08-03 15:55:31 +0100

    Generated a login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
